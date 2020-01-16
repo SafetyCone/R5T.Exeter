e3919b0e8d2822344c51f1897a781d2414088bc1
MinexAutomation <minex.automation@gmail.com>
2020-01-16 14:11:24 -0800

    Moved AddConfiguration() IServiceCollection extension method from R5T.Richmond.

---
+++ source/R5T.Exeter/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace R5T.Exeter
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// The standard way of adding and <see cref="IConfiguration"/> instance, as a singleton.
+        /// </summary>
+        public static IServiceCollection AddConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSingleton<IConfiguration>(configuration);
+
+            return services;
+        }
+    }
+}
