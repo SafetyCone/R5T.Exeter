0151ab660b8be2fb172e7c27d8319e4c054ed93c
MinexAutomation <minex.automation@gmail.com>
2020-01-16 14:12:40 -0800

    Added ServiceProviderHelper extensions.

---
+++ source/R5T.Exeter/Code/Extensions/ServiceProviderHelperExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Dacia.Extensions;
+
+
+namespace R5T.Exeter
+{
+    public static class ServiceProviderHelperExtensions
+    {
+        public static IServiceProvider GetServiceProvider(IConfiguration configuration, Action<ILoggingBuilder> configureLoggingAction, Action<IServiceCollection> configureServicesAction)
+        {
+            var serviceProvider = new ServiceCollection()
+                .AddConfiguration(configuration)
+                .AddLogging(configureLoggingAction)
+                .AddServices(configureServicesAction)
+
+                .BuildServiceProvider();
+
+            return serviceProvider;
+        }
+    }
+}
