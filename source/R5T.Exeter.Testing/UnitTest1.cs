8c23e242abcfbe330a706905ad6afafb25f188cc
MinexAutomation <minex.automation@gmail.com>
2020-01-16 12:31:26 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Exeter.Testing/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace R5T.Exeter.Testing
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
